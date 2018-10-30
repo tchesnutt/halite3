758f95b33ea041ef6be3a57525032e9f35f22c89
tchesnutt <taylor.chesnutt@gmail.com>
2018-10-29 18:30:22 -0700

    init

---
+++ target/debug/deps/rand_core-a62b407f7dbb4b96.d
@@ -0,0 +1,9 @@
+/home/taylorchesnutt/Documents/halite3/target/debug/deps/librand_core-a62b407f7dbb4b96.rlib: /home/taylorchesnutt/.cargo/registry/src/github.com-1ecc6299db9ec823/rand_core-0.3.0/src/lib.rs /home/taylorchesnutt/.cargo/registry/src/github.com-1ecc6299db9ec823/rand_core-0.3.0/src/error.rs /home/taylorchesnutt/.cargo/registry/src/github.com-1ecc6299db9ec823/rand_core-0.3.0/src/block.rs /home/taylorchesnutt/.cargo/registry/src/github.com-1ecc6299db9ec823/rand_core-0.3.0/src/impls.rs /home/taylorchesnutt/.cargo/registry/src/github.com-1ecc6299db9ec823/rand_core-0.3.0/src/le.rs
+
+/home/taylorchesnutt/Documents/halite3/target/debug/deps/rand_core-a62b407f7dbb4b96.d: /home/taylorchesnutt/.cargo/registry/src/github.com-1ecc6299db9ec823/rand_core-0.3.0/src/lib.rs /home/taylorchesnutt/.cargo/registry/src/github.com-1ecc6299db9ec823/rand_core-0.3.0/src/error.rs /home/taylorchesnutt/.cargo/registry/src/github.com-1ecc6299db9ec823/rand_core-0.3.0/src/block.rs /home/taylorchesnutt/.cargo/registry/src/github.com-1ecc6299db9ec823/rand_core-0.3.0/src/impls.rs /home/taylorchesnutt/.cargo/registry/src/github.com-1ecc6299db9ec823/rand_core-0.3.0/src/le.rs
+
+/home/taylorchesnutt/.cargo/registry/src/github.com-1ecc6299db9ec823/rand_core-0.3.0/src/lib.rs:
+/home/taylorchesnutt/.cargo/registry/src/github.com-1ecc6299db9ec823/rand_core-0.3.0/src/error.rs:
+/home/taylorchesnutt/.cargo/registry/src/github.com-1ecc6299db9ec823/rand_core-0.3.0/src/block.rs:
+/home/taylorchesnutt/.cargo/registry/src/github.com-1ecc6299db9ec823/rand_core-0.3.0/src/impls.rs:
+/home/taylorchesnutt/.cargo/registry/src/github.com-1ecc6299db9ec823/rand_core-0.3.0/src/le.rs:
---
+++ target/rls/debug/deps/rand_core-bf9ef8a1101dd72f.d
@@ -0,0 +1,5 @@
+/home/taylorchesnutt/Documents/halite3/target/rls/debug/deps/rand_core-bf9ef8a1101dd72f.rmeta: /home/taylorchesnutt/.cargo/registry/src/github.com-1ecc6299db9ec823/rand_core-0.2.2/src/lib.rs
+
+/home/taylorchesnutt/Documents/halite3/target/rls/debug/deps/rand_core-bf9ef8a1101dd72f.d: /home/taylorchesnutt/.cargo/registry/src/github.com-1ecc6299db9ec823/rand_core-0.2.2/src/lib.rs
+
+/home/taylorchesnutt/.cargo/registry/src/github.com-1ecc6299db9ec823/rand_core-0.2.2/src/lib.rs:
