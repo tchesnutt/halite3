bcaff6df5f4858b840839b1d19999fe2368b21ee
tchesnutt <taylor.chesnutt@gmail.com>
2018-12-09 15:58:57 -0800

    gradient map

--- src/utils/gradient_map.rs
+++ src/utils/gradient_map.rs
@@ -7,9 +7,33 @@ pub struct GradientMap {
 }
 
 impl GradientMap {
-   pub fn initialize(&self, gm: GameMap) -> GradientMap {
-       let mut cells: Vec<Vec<GradientCell>> = Vec::with_capacity(gm.height);
+    pub fn initialize(&self, gm: GameMap) -> GradientMap {
+        //dup gamemap with GradienCells
+        let height = gm.height;
+        let width = gm.width;
 
-       // do gradient work
-   }
+        let mut cells: Vec<Vec<GradientCell>> = Vec::with_capacity(height);
+        for y in 0..cells.len() {
+            let mut row: Vec<GradientCell> = Vec::with_capacity(width);
+            for x in 0..row.len() {
+                let position = Position {
+                    x: x as i32,
+                    y: y as i32,
+                };
+                let value: i32 = gm.at_position(position).halite;
+                // do gradient work
+
+                let cell = GradientCell { position, value };
+                row.push(cell);
+            }
+        }
+
+        GradientMap {
+            width,
+            height,
+            cells,
+        }
+    }
+
+    pub fn adjust(&self) -> GradientCell {}
 }
