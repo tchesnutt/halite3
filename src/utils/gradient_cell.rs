d4f206f0c29729834f9cfabfd8d527511a245619
Taylor Chesnutt <taylor.chesnutt@gmail.com>
2019-01-12 14:18:30 -0800

    added function for adjusting gradient map for ships

--- src/utils/gradient_cell.rs
+++ src/utils/gradient_cell.rs
@@ -3,6 +3,7 @@ use hlt::position::Position;
 pub struct GradientCell {
     pub position: Position
     pub value: f64
+    pub nearby_ship_count: i8
 }
 
 
--- src/utils/gradient_map.rs
+++ src/utils/gradient_map.rs
@@ -7,7 +7,7 @@ pub struct GradientMap {
 }
 
 impl GradientMap {
-    pub fn initialize(&self, gm: GameMap) -> GradientMap {
+    pub fn initialize(&self, game: Game, gm: GameMap, players: Vec<Player>) -> GradientMap {
         //dup gamemap with GradienCells
         let height = gm.height;
         let width = gm.width;
@@ -19,14 +19,15 @@ impl GradientMap {
                 let position = Position {
                     x: x as i32,
                     y: y as i32,
-                };
-                let value: i32 = gm.at_position(position).halite;
-                // do gradient work
-
+                }
+                let value: i32 = gm.at_position(position).halite / 4;
                 let cell = GradientCell { position, value };
                 row.push(cell);
             }
         }
+
+        adjust_cells_for_ship_entities(game, cells);
+
 
         GradientMap {
             width,
@@ -35,5 +36,35 @@ impl GradientMap {
         }
     }
 
-    pub fn adjust(&self) -> GradientCell {}
+
+    
+    fn adjust_cells_for_ship_entities(&self, game: Game, gradient_map: Vec<Vec<GradientCell>>) {
+        // for each ship
+        for (_, ship) in game.ships {
+            for ship_id in player.ship_ids {
+                //loop over 4-radius and increase ship_count on gradient cell
+                for j in -4..4 {
+                    for i in -4..4 {
+                        let current_position = Position {
+                            x: ship.position.x + i as i32,
+                            y: ship.position.y + j as i32,
+                        }
+
+                        gradient_map[current_position.y][current_position.x].nearby_ship_count += 1;
+                    }
+                }
+            }
+        }
+
+        // for each gradient cell increase value if nearby_ship_count is greater than 2
+        for cell in gradient_map.iter().flatten {
+            if cell.nearby_ship_count > 1 {
+                cell.value = cell.value + cell.value * 2;
+            }
+        }
+    }
+
+    // pub fn adjuist_for_my_entity(&self) -> GradientCell {
+
+    // }
 }
